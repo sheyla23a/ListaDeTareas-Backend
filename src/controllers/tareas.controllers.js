2faf4f93fa1b7c65aef96a8d747d629df0506f51
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-17 11:42:27 -0300

    cree la funcion crear tarea haciendo la conexion con postman para crear una nueva tarea y darle de alta

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -5,3 +5,14 @@ export const listarTareas = (req, res) => {
   res.send("Enviar lista de tareas...");
 };
 
+export const crearTarea = async(req,res)=>{
+  try{
+   console.log(req.body)
+   const tareaNueva= new Tarea(req.body);
+   await tareaNueva.save();
+   res.status(201).json({mensaje:'La tarea fue creada correctamente'})
+  }catch(error){
+ console.log(error)
+ res.status(400).json({mensaje: 'No se pudo procesar la solicitud de crear tarea'})
+  }
+}
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { listarTareas } from "../controllers/tareas.controllers.js";
+import { crearTarea, listarTareas } from "../controllers/tareas.controllers.js";
 
 const router = Router();
 
-router.route('/tareas').get(listarTareas);
+router.route('/tareas').get(listarTareas).post(crearTarea)
 
 export default router;
