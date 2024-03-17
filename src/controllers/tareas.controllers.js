ee6d686f0cfa1493e3445475dc95c7585382e121
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-17 15:37:13 -0300

    agregue la funcion para poder editar una tarea

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -32,3 +32,17 @@ export const obtenerTarea = async (req,res)=>{
     res.status(404).json({mensaje: 'No se encontro la tarea solicitada'})
   }
  }
+
+ export const editarTarea = async(req,res)=>{
+  try{
+   const buscarTarea = await Tarea.findById(req.params.id);
+   if(!buscarTarea){
+     return res.status(404).json({mensaje:'No se pudo editar la tarea, el id es incorrecto'})
+   }
+   await Tarea.findByIdAndUpdate(req.params.id, req.body);
+   res.status(200).json({mensaje:'La tarea fue modificada exitosamente'});
+  }catch(error){
+      console.error(error)
+      res.status(500).json({mensaje:'Ocurrio un error al intentar editar la tarea'})
+  }
+  }
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearTarea, listaTareas, obtenerTarea } from "../controllers/tareas.controllers.js";
+import { crearTarea, editarTarea, listaTareas, obtenerTarea } from "../controllers/tareas.controllers.js";
 
 const router = Router();
 
 router.route('/tareas').get(listaTareas).post(crearTarea)
-router.route('/tarea/:id').get(obtenerTarea)
+router.route('/tarea/:id').get(obtenerTarea).put(editarTarea)
 
 export default router;
